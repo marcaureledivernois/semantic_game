74b84d2ff9743cf093b4ec715cde60f3d50c6ee2
marcaureledivernois <divernois@gmail.com>
2022-04-26 19:05:21 +0200

    adding streamlit app script

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ with open('embeddings_matrix.npy', 'rb') as f:
     embeddings_matrix = np.load(f, allow_pickle=True)
 
 st.title('Guess the secret word !')
-st.write('Play the game [here](https://share.streamlit.io/marcaureledivernois/semantic_game/app.py). The goal of the game is to guess a secret word. To do so,'
-         'propose any word and see how close you are from the secret word. The higher the score you get, the closer'
-         'you are to the secret word. When you score high, it means your proposed word is either easily'
+st.write('Play the game . The goal of the game is to guess a secret word. To do so, '
+         'propose any word and see how close you are from the secret word. The higher the score you get, the closer '
+         'you are to the secret word. When you score high, it means your proposed word is either easily '
          'interchangeable with the secret word, or that it is often associated with the secret word.')
 
 if st.button('New Secret Word'):
@@ -31,13 +31,14 @@ if st.button('New Secret Word'):
     distances = np.c_[distances, [words[int(distances[i, 0])] for i in range(len(distances))]]
 
     proposed_word = st.text_input('Enter your proposed word')
-
-    if proposed_word in words:
-        rank = np.where(distances[:,2]==proposed_word)
-        st.write('Your proposed word is ranked',rank ,'in the closest words !')
-        if rank == 0:
-            st.write('Congratulations !! You found the secret word !')
-            st.write('It is', target_word)
-    else:
-        st.write('I dont know this word :( Please select another one')
+    
+    if proposed_word:
+        if proposed_word in words:
+            rank = np.where(distances[:,2]==proposed_word)
+            st.write('Your proposed word is ranked',rank ,'in the closest words !')
+            if rank == 0:
+                st.write('Congratulations !! You found the secret word !')
+                st.write('It is', target_word)
+        else:
+            st.write('I dont know this word :( Please select another one')
 
