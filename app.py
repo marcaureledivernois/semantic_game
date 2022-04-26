bf9d0e5b115ee13c5053503c71532a0fb1c28932
marcaureledivernois <divernois@gmail.com>
2022-04-26 20:50:40 +0200

    adding streamlit app script

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ proposed_word = st.text_input('Enter your proposed word')
 if proposed_word:
     if proposed_word in words:
         rank = np.where(distances[:,2]==proposed_word)[0][0]
-        distance = distances[rank,1][0]
-        st.write(proposed_word ,'is', float(distance),'unit away from the target word ! It is the',rank ,'closest word !')
+        distance = float(distances[rank,1][0])
+        st.write(proposed_word ,'is', round(distance,3),'unit away from the target word ! It is the',rank ,'closest word !')
         store_inputs().append({'Proposed Word': proposed_word,'Score': distance,'Rank' : rank})
 
         if rank == 0:
